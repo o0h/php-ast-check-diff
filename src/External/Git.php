e5b79be7591a5a040c126504e8f92a2faac3099a
o0h <dev@o0h.in>
2023-12-24 15:49:52 +0900

    Improve Git functionality and add command execution safety
    Implemented a new shellExec method to securely handle arguments of shell commands, reducing potential security risks. Additionally, built a constructor to verify if git command is installed at instantiation time, throwing an error if it's not, thereby ensuring proper operation of the Git class.

--- src/External/Git.php
+++ src/External/Git.php
@@ -12,6 +12,16 @@ use O0h\PhpAstCheckDiff\Value\GitStatus;
  */
 final class Git implements GitInterface
 {
+    private const UNESCAPE_SHELL_ARG_PREFIX = '@@@@';
+
+    public function __construct()
+    {
+        $gitInstalled = $this->shellExec('which git');
+        if (!$gitInstalled) {
+            throw new \RuntimeException('git command is not installed');
+        }
+    }
+
     #[\Override]
     public function verifyCommit(string $commitHash): bool
     {
@@ -61,11 +71,37 @@ final class Git implements GitInterface
     #[\Override]
     public function getSource(string $commitHash, string $path): string
     {
-        $source = shell_exec(sprintf('git show %s:"%s"', escapeshellarg($commitHash), $path));
+        $source = $this->shellExec(
+            'git show %s:"%s"',
+            $commitHash,
+            self::UNESCAPE_SHELL_ARG_PREFIX . $path
+        );
         if (!$source) {
             throw new \UnexpectedValueException("Failed to load {$commitHash}:{$path}");
         }
 
         return $source;
+    }
+
+    /**
+     * Executes a shell command with given arguments.
+     *
+     * @param string $externalCommand the placeholder string with format specifiers
+     * @param string ...$args The variable number of arguments to substitute in the placeholder.
+     *
+     * @return null|false|string returns the output of the shell command as a string, or null if it fails
+     */
+    private function shellExec(string $externalCommand, string ...$args): null|false|string
+    {
+        if ($args) {
+            $px = self::UNESCAPE_SHELL_ARG_PREFIX;
+            $args = array_map(
+                static fn($arg): string => str_starts_with($arg, $px) ? substr($arg, \strlen($px)) : escapeshellarg($arg),
+                $args
+            );
+            $externalCommand = sprintf($externalCommand, ...$args);
+        }
+
+        return shell_exec($externalCommand);
     }
 }
